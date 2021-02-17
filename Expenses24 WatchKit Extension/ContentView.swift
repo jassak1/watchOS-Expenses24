67f940406b2e648ee26211a0785df569bf700bd4
Jassak <jassak@me.com>
2021-02-17 10:46:00 +0100

    Initial Commit

---
+++ Expenses24 WatchKit Extension/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Expenses24
+//
+//  Created by jassak on 17/02/2021.
+//  Copyright © 2021 jassak1. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Expenses24 WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Expenses24
+//
+//  Created by jassak on 17/02/2021.
+//  Copyright © 2021 jassak1. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
